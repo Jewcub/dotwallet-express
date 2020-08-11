e5957cede40b6e4826b24738615331ebfbead996
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-08-11 14:38:27 +0800

    typs declaration location. jsnotes

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # dotwallet-express
- express package for interacting with dotwallet APIs
+
+express package for interacting with dotwallet APIs
+
+## to use
+
+```bash
+npm install dotwallet-express
+```
+
+```js
+const express = require('express');
+const app = express();
+
+import DotWallet from 'dotwallet-express';
+const dotwallet = new DotWallet(YOUR_APP_ID, YOUR_APP_SECRET);
+
+// send the authentication response
+app.get(
+  '/auth',
+  dotwallet.handleAuthResponse('/restricted-page/').then((result) => {
+    const userData = result.userData;
+    const refreshToken = result.accessData.refresh_token;
+  }),
+);
+// refresh access token
+dotwallet.refreshToken(refreshToken);
+```
--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ class DotWallet {
     this.SECRET = secret;
   }
 
+  /** @param redirectWithQueries If you'd like to have express send the response to redirect to a url, carrying the user data queries with it, than include a redirect URL string here
+   * @param log whether to log events
+   * @example app.get('/auth', Dotwallet.handleAuthResponse('/frontend-landing-page', true)); app.get('/auth', dotwallet.handleAuthResponse().then(result=>{const userData = result.userData; const accessToken = result.accessData.access_token;}));
+   * @returns {object} { userData: {user_open_id, ...}, accessData:{ refresh_token, expires_in, access_token }}
+   */
   handleAuthResponse = (redirectWithQueries?: string, log?: boolean) => {
     return async (req: Request, res: Response) => {
       // console.log('req, res', req, res);
@@ -24,10 +29,10 @@ class DotWallet {
 
         const accessTokenRequest = await axios.post('https://www.ddpurse.com/platform/openapi/access_token', data);
         if (log) console.log('==============access token result==============\n', accessTokenRequest.data);
-        const accessToken = accessTokenRequest.data.data.access_token;
-        if (accessToken) {
+        const accessData = accessTokenRequest.data.data;
+        if (accessData.access_token) {
           const userInfoRequest = await axios.get(
-            'https://www.ddpurse.com/platform/openapi/get_user_info?access_token=' + accessToken,
+            'https://www.ddpurse.com/platform/openapi/get_user_info?access_token=' + accessData.access_token,
           );
           if (log) console.log('==============user info result==============\n', userInfoRequest.data);
           const userData = userInfoRequest.data.data;
@@ -38,7 +43,7 @@ class DotWallet {
                 query: { ...userData },
               }),
             );
-          return { userData, accessToken };
+          return { userData, accessData };
         } else throw accessTokenRequest;
       } catch (err) {
         console.warn('==============ERROR==============\n', err);
